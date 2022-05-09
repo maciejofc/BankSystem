85dc986c9f7e37d2043de6ef7b8fb1fa24cceda0
maciejofc <maciej.ofc.k@gmail.com>
2022-05-10 00:17:52 +0200

    import project

---
+++ src/test/java/pl/maciejowsky/banksystem/BankSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.maciejowsky.banksystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
